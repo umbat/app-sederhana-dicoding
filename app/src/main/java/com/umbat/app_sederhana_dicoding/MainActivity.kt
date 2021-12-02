d08e32e809762a11d5a8d74aad278d4bf91d7190
umbat <farhanpratamax@gmail.com>
2021-12-02 22:12:19 +0700

    Initial commit

---
+++ app/src/main/java/com/umbat/app_sederhana_dicoding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.umbat.app_sederhana_dicoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
