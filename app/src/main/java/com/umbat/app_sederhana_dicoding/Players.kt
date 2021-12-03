1ceb63f36325ee3db2bc40ecc0867447252f53a3
umbat <farhanpratamax@gmail.com>
2021-12-03 22:04:55 +0700

    adding players data

---
+++ app/src/main/java/com/umbat/app_sederhana_dicoding/Players.kt
@@ -0,0 +1,7 @@
+package com.umbat.app_sederhana_dicoding
+
+data class Players(
+    val name: String = "",
+    val detail: String = "",
+    val photo: Int = 0
+)
---
+++ app/src/main/java/com/umbat/app_sederhana_dicoding/PlayersData.kt
@@ -0,0 +1,25 @@
+package com.umbat.app_sederhana_dicoding
+
+object PlayersData {
+    private val playersName = arrayOf (
+        "Wojciech Szczesny",
+        "Matthijs de Ligt",
+        "Leonardo Bonucci",
+        "Alex Sandro",
+        "Danilo",
+        "Manuel Locatelli",
+        "Weston McKennie",
+        "Federico Chiesa",
+        "Federico Bernardeschi",
+        "Paulo Dybala",
+        "Álvaro Morata"
+    )
+
+    private val playersDetails = arrayOf (
+        ""
+    )
+
+    private val playersPhoto = arrayOf (
+        ""
+    )
+}
