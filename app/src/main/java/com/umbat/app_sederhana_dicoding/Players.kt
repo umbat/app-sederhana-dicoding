59256d753efd6ea7d93068b3108c816143961880
umbat <farhanpratamax@gmail.com>
2021-12-17 23:35:59 +0700

    adding the data of players in PlayersData.kt

--- app/src/main/java/com/umbat/app_sederhana_dicoding/Players.kt
+++ app/src/main/java/com/umbat/app_sederhana_dicoding/Players.kt
@@ -2,6 +2,10 @@ package com.umbat.app_sederhana_dicoding
 
 data class Players(
     var name: String = "",
+    var fullname: String = "",
+    var placeofbirth: String = "",
+    var timeofbirth: String = "",
+    var position: String = "",
     var detail: String = "",
     var photo: Int = 0
 )
--- app/src/main/java/com/umbat/app_sederhana_dicoding/PlayersData.kt
+++ app/src/main/java/com/umbat/app_sederhana_dicoding/PlayersData.kt
@@ -15,18 +15,74 @@ object PlayersData {
         "Álvaro Morata"
     )
 
+    private val playersFullNames = arrayOf (
+        "Wojciech Tomasz Szczęsny",
+        "Matthijs de Ligt",
+        "Leonardo Bonucci",
+        "Alex Sandro Lobo Silva",
+        "Danilo Luiz da Silva",
+        "Manuel Locatelli",
+        "Weston James Earl McKennie",
+        "Federico Chiesa",
+        "Federico Bernardeschi",
+        "Paulo Bruno Exequiel Dybala",
+        "Álvaro Borja Morata Martín"
+    )
+
+    private val playersPlaceOfBirth = arrayOf (
+        "Warszawa, Poland",
+        "Leiderdorp, Netherlands",
+        "Viterbo, Italy",
+        "Catanduva, Brazil",
+        "Bicas, Brazil",
+        "Lecco, Italy",
+        "Little Elm, Texas, USA",
+        "Genova, Italy",
+        "Carrara, Italy",
+        "Laguna Larga, Argentine",
+        "Madrid, Spain"
+    )
+
+    private val playersTimeOfBirth = arrayOf (
+        "April 18, 1990",
+        "August 12, 1999",
+        "May 1, 1987",
+        "January 26, 1991",
+        "July 15, 1991",
+        "January 8, 1998",
+        "August 28, 1998",
+        "October 25, 1997",
+        "February 16, 1994",
+        "November 15, 1993",
+        "October 23, 1992"
+    )
+
+    private val playersPosition = arrayOf (
+        "Goalkeeper",
+        "Center Back",
+        "Center Back",
+        "Left Wing Back",
+        "Right Wing Back",
+        "Midfielder",
+        "Midfielder",
+        "Right Winger",
+        "Left Winger",
+        "Second Striker",
+        "Striker"
+    )
+
     private val playersDetails = arrayOf (
-        "Wojciech Tomasz Szczęsny (pronounced [ˈvɔjt͡ɕɛx ˈʂt͡ʂɛ̃snɨ]; born 18 April 1990) is a Polish professional footballer who plays as a goalkeeper for Serie A club Juventus and the Poland national team",
-        "Matthijs de Ligt (Dutch pronunciation: [mɑˈtɛiz də ˈlɪxt]; born 12 August 1999) is a Dutch professional footballer who plays as a centre-back for Serie A club Juventus and the Netherlands national team",
-        "Leonardo Bonucci (Italian pronunciation: [leoˈnardo boˈnuttʃi]; born 1 May 1987) is an Italian professional footballer who plays as a centre-back for Serie A club Juventus and the Italy national team",
-        "Alex Sandro Lobo Silva (born 26 January 1991) is a Brazilian professional footballer who plays as a left-back for Serie A club Juventus and the Brazil national team",
-        "Danilo Luiz da Silva (born 15 July 1991), known as Danilo, is a Brazilian professional footballer who plays as a right-back for Serie A club Juventus and the Brazil national team",
-        "Manuel Locatelli (born 8 January 1998) is an Italian professional footballer who plays as a midfielder for Serie A club Juventus, on loan from Serie A club Sassuolo, and the Italy national team",
-        "Weston James Earl McKennie (born August 28, 1998) is an American professional soccer player who plays as a midfielder for Serie A club Juventus and the United States national team",
-        "Federico Chiesa (Italian pronunciation: [fedeˈriːko ˈkjɛːza; ˈkjeːza]; born 25 October 1997) is an Italian professional footballer who plays as a winger for Serie A club Juventus, on loan from Fiorentina, and the Italy national team",
-        "Federico Bernardeschi (Italian pronunciation: [fedeˈriːko bernarˈdeski]; born 16 February 1994) is an Italian professional footballer who plays as a winger or attacking midfielder for Serie A club Juventus and the Italy national team",
-        "Paulo Exequiel Dybala (born 15 November 1993) is an Argentine professional footballer who plays as a forward for Serie A club Juventus and the Argentina national team",
-        "Álvaro Borja Morata Martín (Spanish pronunciation: [ˈalβaɾo moˈɾata]; born 23 October 1992) is a Spanish professional footballer who plays as a striker for Serie A club Juventus, on loan from La Liga club Atlético Madrid, and the Spain national team"
+        "Wojciech Tomasz Szczęsny (pronounced [ˈvɔjt͡ɕɛx ˈʂt͡ʂɛ̃snɨ]) is a Polish professional footballer who plays as a goalkeeper for Serie A club Juventus and the Poland national team. After starting his club career at Legia Warsaw, Szczęsny signed for Arsenal in January 2006, where he made his professional debut in 2009. After a spell on loan with Brentford the following season, he later became the capital club's first choice goalkeeper, winning two FA Cups and being the joint recipient of the 2013–14 Premier League Golden Glove with Petr Čech. He subsequently went on two consecutive season-long loans to Italian club Roma in 2015, where his performances earned him a move to reigning league champions Juventus in 2017. After serving mainly as a back-up to Gianluigi Buffon in his first season, during which he won a domestic double, he inherited the starting spot from the departed Buffon the following season, winning his second Serie A title; his performances also earned him a Yashin Trophy nomination in 2019. He won a third consecutive league title the following season, also winning the Serie A Best Goalkeeper Award.",
+        "Matthijs de Ligt (Dutch pronunciation: [mɑˈtɛiz də ˈlɪxt]) is a Dutch professional footballer who plays as a centre-back for Serie A club Juventus and the Netherlands national team. On 21 September 2016, De Ligt made his debut for Ajax's senior team in a cup game against Willem II. He scored from a corner after 25 minutes, making him the second-youngest goalscorer ever behind Clarence Seedorf. On 24 May, he became the youngest player ever (17 years and 285 days old) to play in a major European final when he started against Manchester United in the 2017 UEFA Europa League Final. On 17 December 2018, De Ligt won the Golden Boy award, becoming the first defender to win the award. The following season, he helped Ajax capture a domestic double and reach the semi-finals of the Champions League; his performances earned him a move to Serie A side Juventus, and saw him win the Kopa Trophy in 2019. He won the Serie A title during his first season with the Turin side.",
+        "Leonardo Bonucci (Italian pronunciation: [leoˈnardo boˈnuttʃi]) is an Italian professional footballer who plays as a centre-back for Serie A club Juventus and the Italy national team. After beginning his career with Inter Milan in 2005, Bonucci spent the next few seasons on loan at Treviso and Pisa, before moving to Bari in 2009. His technique, ball-playing ability and defensive performances alongside fellow Italian centre-back Andrea Ranocchia earned him a move to Juventus the following season, where he later became a key member of the club's three-man defensive line, alongside Giorgio Chiellini and Andrea Barzagli, establishing himself as one of the best defenders in world football. Among other titles, he went on to win six consecutive Serie A titles with the team between 2012 and 2017, having also played two UEFA Champions League finals in 2015 and 2017. In 2017, he moved to AC Milan, and one season later returned to Juventus, winning two more consecutive league titles.",
+        "Alex Sandro Lobo Silva is a Brazilian professional footballer who plays as a left-back for Serie A club Juventus and the Brazil national team. At club level, Alex Sandro began his career with Atletico Paranaense, and later also played for Santos on loan. In 2011, he joined Porto for €9.6 million, alongside teammate and countryman Danilo, who plays as a right back. He joined Juventus in 2015, winning the domestic double in his first three seasons, followed by two more consecutive league titles over the following two years.",
+        "Danilo Luiz da Silva known as Danilo, is a Brazilian professional footballer who plays as a right-back for Serie A club Juventus and the Brazil national team. Danilo began his career with América Mineiro before moving to Santos, where he scored the goal that won the 2011 Copa Libertadores. In January 2012, he moved to Portuguese club Porto, where he won consecutive Primeira Liga titles. In 2015, he joined Real Madrid following a €31.5 million transfer fee. In 2017, he joined Manchester City. In 2019, having won two Premier League titles, an FA Cup and two EFL Cups with City, Danilo joined Juventus, winning the Serie A title during his first season with the club.",
+        "Manuel Locatelli is an Italian professional footballer who plays as a midfielder for Serie A club Juventus, on loan from Serie A club Sassuolo, and the Italy national team. After coming through the club's youth system, Locatelli made his professional debut with AC Milan, helping them win the 2016 Supercoppa Italiana. He moved to Sassuolo in 2018, before joining Juventus in 2021. Locatelli represented Italy internationally, participating in Italy's victorious UEFA Euro 2020 campaign.",
+        "Weston James Earl McKennie (born ) is an American professional soccer player who plays as a midfielder for Serie A club Juventus and the United States national team. Coming through the youth system, McKennie began his senior club career at Bundesliga side Schalke 04. In 2017–18, he established himself as a first team player, playing 25 games in all competitions. After three seasons, McKennie moved to Italian champions Juventus on loan, with the deal becoming permanent in March 2021.",
+        "Federico Chiesa (Italian pronunciation: [fedeˈriːko ˈkjɛːza; ˈkjeːza]) is an Italian professional footballer who plays as a winger for Serie A club Juventus, on loan from Fiorentina, and the Italy national team. He is the son of former footballer Enrico Chiesa. Chiesa began his youth career with Settignanese from Settignano, a frazione northeast of Florence, where he was coached by Fiorentina legend Kurt Hamrin. He joined Fiorentina in 2007. He made his competitive debut for Fiorentina in a 2–1 away defeat against Juventus, during the opening fixture of the 2016–17 Serie A season. On 16 September 2017, in the 2017–18 season, Chiesa marked his 30th league appearance for Fiorentina with a goal in the Derby dell'Appennino against Bologna. On 5 October 2020, Chiesa signed a two-year loan deal with Juventus; a €3 million loan for the first season and a €7 million loan for the second season, with a conditional obligation to buy for €40 million plus €10 million in variables.",
+        "Federico Bernardeschi (Italian pronunciation: [fedeˈriːko bernarˈdeski]) is an Italian professional footballer who plays as a winger or attacking midfielder for Serie A club Juventus and the Italy national team. Bernardeschi began his playing career with Atletico Carrara and Polisportiva Ponzano. In 2003, he moved to the Fiorentina youth system, where he spent 10 years, before spending one season on loan at Crotone during the 2013–14 season. He returned to Fiorentina the following season. In July 2017, he was sold to Juventus, where he has since won three consecutive Serie A titles, among other trophies.",
+        "Paulo Exequiel Dybala is an Argentine professional footballer who plays as a forward for Serie A club Juventus and the Argentina national team. Nicknamed \"La Joya\" (\"The Jewel\"),[4] Dybala began his senior club career in 2011 playing for Instituto de Córdoba, before signing for Palermo in 2012, at age 18, where he won a Serie B title. In 2015, Dybala signed for Juventus and won five league titles and four Coppas Italia. He was also voted Serie A Most Valuable Player once, was also included in the Serie A Team of the Year four times, and ranks as Juventus' eleventh-highest all-time goalscorer.",
+        "Álvaro Borja Morata Martín (Spanish pronunciation: [ˈalβaɾo moˈɾata]) is a Spanish professional footballer who plays as a striker for Serie A club Juventus, on loan from La Liga club Atlético Madrid, and the Spain national team. He began his career at Real Madrid, making his debut with the senior team in late 2010. After winning the 2013–14 UEFA Champions League, he moved to Juventus for €20 million in 2014, winning the double of Serie A and the Coppa Italia in both of his seasons in Turin. After being bought back by Real for €30 million, he won another La Liga and the UEFA Champions League in 2016–17 before joining Chelsea in 2017 for a club record fee of around £60 million. In January 2019, he moved to Atlético Madrid on loan and joined the club permanently on 1 July 2020. Morata moved back to Juventus on loan in 2020."
     )
 
     private val playersPhoto = intArrayOf(
@@ -48,6 +104,10 @@ object PlayersData {
         for (position in playersNames.indices){
             val players = Players()
             players.name = playersNames[position]
+            players.fullname = playersFullNames[position]
+            players.placeofbirth = playersPlaceOfBirth[position]
+            players.timeofbirth = playersTimeOfBirth[position]
+            players.position = playersPosition[position]
             players.detail = playersDetails[position]
             players.photo = playersPhoto[position]
             list.add(players)
