a823db971d1bc1bc4e35a7aac72c2666c3550d83
umbat <farhanpratamax@gmail.com>
2021-12-19 15:13:28 +0700

    adding SplashActivity.kt

---
+++ app/src/main/java/com/umbat/app_sederhana_dicoding/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.umbat.app_sederhana_dicoding
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val TIMEOUT = 1000L
+
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIMEOUT)
+    }
+}
